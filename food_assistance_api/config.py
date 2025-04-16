2c1064a92d0bdb6f24cabedb316e75e2a6d761e1
Joyson Gaurea <jng05929@gmail.com>
2025-04-15 21:37:10 -0400

    updated gitignore

---
+++ food_assistance_api/config.py
@@ -0,0 +1,4 @@
+API_KEY = "AIzaSyDuG-5uLRNqwrV6jji8GU0s-ytsCRb_-RU"
+DATABASE_URL = "sqlite:///food_assistance.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:///food_assistance.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
